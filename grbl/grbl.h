ed3f2600340a0d94a66fee624985c84d0a5d2e2d
Terje Io <terjeio@online.no>
2018-07-04 09:36:51 +0200

    Corrected version string, was changed during testing

--- grbl/grbl.h
+++ grbl/grbl.h
@@ -22,7 +22,7 @@
 #define grbl_h
 
 // Grbl versioning system
-#define GRBL_VERSION "1.1g"
+#define GRBL_VERSION "1.1f"
 #define GRBL_VERSION_BUILD "20170802"
 
 // Define standard libraries used by Grbl.
